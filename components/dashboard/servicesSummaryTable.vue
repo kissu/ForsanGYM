df44b7a3230b0f110795c1900ac2134c636e3823
Youssef <youssef.afify122@gmail.com>
2021-07-04 14:12:45 +0200

    help u

---
+++ components/dashboard/servicesSummaryTable.vue
@@ -0,0 +1,48 @@
+<template>
+  <table style="font-size:17px"  class="table table-striped">
+    <thead>
+    <tr>
+      <th>#</th>
+      <th>Type of service</th>
+      <th>Number of paid items</th>
+      <th>Total income</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+      <td>1</td>
+      <td>T-Shirt</td>
+      <td>0</td>
+      <td>0</td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>Chicken meal</td>
+      <td>2</td>
+      <td>90</td>
+    </tr>
+    <tr>
+      <td>3</td>
+      <td>Oats meal</td>
+      <td>4</td>
+      <td>60</td>
+    </tr>
+    <tr>
+      <td>4</td>
+      <td>Water</td>
+      <td>15</td>
+      <td>45</td>
+    </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: "servicesSummaryTable"
+}
+</script>
+
+<style scoped>
+
+</style>
