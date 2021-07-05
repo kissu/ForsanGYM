4ceb516be5a5457cd38c93460a72ac6a2697b859
Youssef <youssef.afify122@gmail.com>
2021-07-05 13:45:51 +0200

    add pageTitle component with props for feature talk

---
+++ components/layout/pageTitle.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="app-title">
+    <div>
+      <h1><i :class="icon+' mr-1'"></i>{{title}}</h1>
+    </div>
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "pageTitle",
+  props: {
+    title: {
+      required: true,
+    },
+    icon:  {
+      required: true,
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
