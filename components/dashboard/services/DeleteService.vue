adb13c883dd1985123584fe8e3d19b8d86047134
Abdullah3553 <abdullah.ayoub2000@gmail.com>
2021-07-17 03:30:09 +0200

    warning msg added

--- components/dashboard/services/DeleteService.vue
+++ components/dashboard/services/DeleteService.vue
@@ -3,15 +3,16 @@
 
   <CollapseComponent btns-class="btn btn-danger" head-btn-text="Delete Service" foot-btn-text="Delete" CollapseName="DeleteService">
     <div class="form-group">
-      <label for="ServiceSelect">Select Service</label>
+      <label for="ServiceSelect">Select The Service</label>
       <select class="form-control" id="ServiceSelect">
-        <option>1</option>
-        <option>2</option>
-        <option>3</option>
-        <option>4</option>
-        <option>5</option>
+        <option>Service 1</option>
+        <option>Service 2</option>
+        <option>Service 3</option>
+        <option>Service 4</option>
+        <option>Service 5</option>
       </select>
     </div>
+    <p >Warning : Delete Operation is <b style="color: #96000e">IRREVERSIBLE</b>.</p>
   </CollapseComponent>
 
 </div>
