540dbd12a66f068f1bea80b0bc21f7dcdb12e32c
AhmedGamal77823 <ahmedgamale.1019@gmail.com>
2021-07-22 16:16:12 +0200

    activities page finished!

--- store/index.js
+++ store/index.js
@@ -38,6 +38,15 @@ export const mutations = {
   },
   addNewActivityPlayer: function(state, activityPlayer){
     state.activityPlayers.push(activityPlayer)
+  },
+  editActivity: function(state, act){
+    console.log("Hi");
+    let objIndex = state.activities.findIndex((obj => obj.id == act.id))
+    state.activities[objIndex].name = act.name,
+    state.activities[objIndex].coachName = act.coachName,
+    state.activities[objIndex].coachPhoneNumber = act.coachPhoneNumber,
+    state.activities[objIndex].price = act.price,
+    state.activities[objIndex].description = act.description
   }
 }
 
