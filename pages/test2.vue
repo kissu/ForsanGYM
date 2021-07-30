bf9b6b0107aba041f5183166bff218334e5cc454
Abdullah3553 <abdullah.ayoub2000@gmail.com>
2021-07-31 01:47:52 +0200

    tessssssssst

---
+++ pages/test2.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+  <h1>This is test 2</h1>
+  {{element}}
+</div>
+</template>
+
+<script>
+export default {
+  name: "test2.vue",
+  computed:{
+    element:function (){
+      return this.$route.query.element
+    }
+  },
+  created() {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
