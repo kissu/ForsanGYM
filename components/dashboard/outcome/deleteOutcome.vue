e26a07524aebb76aed1e56e4f9583b4e96e0ea0f
Abdullah3553 <abdullah.ayoub2000@gmail.com>
2021-08-18 03:29:43 +0200

    delete outcome added

---
+++ components/dashboard/outcome/deleteOutcome.vue
@@ -0,0 +1,89 @@
+<template>
+  <div id="deleteOutcomeCom">
+
+    <CollapseComponent btns-class="btn btn-danger" head-btn-text="Delete Outcome"
+                       CollapseName="DeleteOutcome" icon="mdi mdi-trash-can" >
+      <div class="form-group">
+        <label for="outcomeSelect">Select The Outcome</label>
+        <select class="form-control" id="outcomeSelect" v-bind:value="SelectedOutcome"
+                @input="SelectedOutcome = $event.target.value">
+          <option :value="null" disabled selected>Choose the Outcome</option>
+          <option v-for="outcome in outcomes" :value="outcome" :key="outcome.id">{{ outcome.name }} --
+            {{ outcome.price }}
+          </option>
+        </select>
+      </div>
+      <p>Warning : Delete Operation is <b style="color: #96000e">IRREVERSIBLE</b>.</p>
+
+      <div class="tile-footer">
+        <div class="row">
+          <div class="col-md-8 ">
+            <button class="btn btn-danger" type="button" data-toggle="collapse" data-target="#DeleteOutcomecollapse"
+                    v-on:click="DeleteOutcome"
+                    >
+              <i class="mdi mdi-check-circle"></i>
+              Delete
+            </button>
+          </div>
+        </div>
+      </div>
+    </CollapseComponent>
+
+  </div>
+</template>
+
+<script>
+import CollapseComponent from "../../layout/Collapse";
+
+export default {
+  name: "DeleteOutcome",
+  components: {CollapseComponent},
+  data() {
+    return {
+      SelectedOutcome: null,
+    }
+  },
+  methods: {
+    DeleteOutcome: function () {
+      this.$swal.fire({
+        title: 'Are you sure you want to delete this outcome ? ',
+        showDenyButton: true,
+        icon: 'question',
+
+        showCancelButton: false,
+        confirmButtonText: `Yes`,
+        denyButtonText: `cancel`,
+      }).then((result) => {
+        /* Read more about isConfirmed, isDenied below */
+        if (result.isConfirmed) {
+          // Delete service from all database
+          this.$axios.$delete('outcome/delete/' + this.SelectedOutcome.id).then(() => {
+            this.$store.commit('DeleteOutcome', this.SelectedOutcome)
+            this.SelectedOutcome = null
+          }).catch(err => {
+            this.$swal.fire({
+              title: "Delete Outcome FAILED",
+              icon: "error",
+              text: err.response.data.message
+            })
+            console.log(err)
+            return false
+          })
+          this.$swal.fire('Outcome Deleted!', '', 'success')
+        }
+      })
+
+
+    }
+  },
+  computed:{
+    outcomes: function (){
+      return this.$store.state.outcomes
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
