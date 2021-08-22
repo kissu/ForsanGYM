32f043a7e2a6776930cff32cf1a0a34a67b0883c
Abdullah <abdullah.ayoub2000@gmail.com>
2021-08-22 21:54:06 +0200

    reset form in delete admin
    كل فشار

--- components/superAdminPanal/deleteAdmin.vue
+++ components/superAdminPanal/deleteAdmin.vue
@@ -15,17 +15,17 @@
           <h4>Choose the Admin you want to Delete</h4>
           <div class="form-group row">
             <div class="col-md-8">
-              <select  class="form-control col-md-8" id="adminList">
+              <select  class="form-control col-md-8" id="adminList" @change="clickedAdmin = admins[$event.target.value]">
                 <option disabled selected>Choose Admin</option>
-                <option v-for="admin in admins" :key="admin.id" @click="clickedAdmin = admin">
+                <option v-for="(admin,index) in admins" :key="admin.id" :value="index">
                   {{admin.username}} -- {{admin.name}}</option>
               </select>
             </div>
           </div>
         </div>
         <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-danger" v-on:click="deleteAdmin">Delete</button>
+          <button type="button" class="btn btn-secondary" data-dismiss="modal" @click="resetForm">Close</button>
+          <button type="button" class="btn btn-danger" data-dismiss="modal" v-on:click="deleteAdmin">Delete</button>
         </div>
       </div>
     </div>
@@ -43,25 +43,27 @@ export default {
     }
   },
   methods:{
-    // test:function (val){
-    //   console.log("tst worked ")
-    //   console.log(val)
-    // },
+    resetForm:function (){
+      this.clickedAdmin = {}
+      document.getElementById('adminList').selectedIndex = 0
+    },
     deleteAdmin: function (){
       // console.log(this.clickedAdmin)
       // return
-      this.$axios.$delete(`auth/deleteAdmin/${this.clickedAdmin.id}`).then(()=>{
-        this.$store.commit('deleteAdmin', this.clickedAdmin)
+      const clickedAdmin = {...this.clickedAdmin}
+      this.$axios.$delete(`auth/deleteAdmin/${clickedAdmin.id}`).then(()=>{
+        this.$store.commit('deleteAdmin', clickedAdmin)
+        this.resetForm()
       }).catch(err=>{
         this.$swal.fire({
-          title:"Error happend while the deleting admin",
+          title:"Error happened while the deleting admin",
           icon:"error"
         })
         console.log("Error in delete admin")
         console.log(err)
+        this.resetForm()
+        return false
       })
-      $(`#deleteAdminModal`).modal('hide')
-      document.getElementById('adminList').selectedIndex = 0
 
     }
   },
